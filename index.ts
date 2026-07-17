b0cca070bbe5fd6fab6a3ae5326ef016670619e7
nadimtuhin <nadimtuhin@gmail.com>
2026-07-17 15:35:11 +0600

    chore: update public API exports for all new modules

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 export { scanUrl, crawlUrls, getChromePath } from './src/scanner';
 export type { ScanOptions, CrawlResult } from './src/scanner';
 export { filterByLevel, parseResults } from './src/filter';
-export { toPlainText, toHtml, toCsv, saveReport, sendWebhook } from './src/reporter';
+export { toPlainText, toHtml, toCsv, toMarkdown, toXml, saveReport, sendWebhook } from './src/reporter';
 export { loadConfig } from './src/config';
 export { openDb, logScan, getHistory } from './src/history';
 export { runLighthouse, mockLighthouseResult } from './src/lighthouse';
@@ -15,3 +15,23 @@ export type { WcagLevel } from './src/filter';
 export type { ParsedResults } from './src/reporter';
 export type { A11yConfig, AuthConfig } from './src/config';
 export type { HistoryRow } from './src/history';
+// New features
+export { auditSpa } from './src/spa';
+export type { SpaAuditResult } from './src/spa';
+export { scanUrlsParallel } from './src/worker-pool';
+export type { WorkerPoolOptions } from './src/worker-pool';
+export {
+  checkAriaRoles,
+  checkFocusTrap,
+  checkTargetSize,
+  checkLangAttr,
+  checkSkipNav,
+  checkColorBlindness,
+  checkLabelAssociation,
+  checkAnimationRate,
+  checkBrokenAria,
+  runExtendedChecks,
+  colorBlindnessFilters,
+} from './src/checks';
+export type { CheckerResult, Finding } from './src/checks';
+export { savePdfReport } from './src/pdf';
